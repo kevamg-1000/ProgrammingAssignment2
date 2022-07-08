6ab3abb6241b8a53b84944d07faac95e66248d7b
kevamg-1000 <chrono.kev@gmail.com>
2022-07-08 19:26:15 +0530

    Finished the second function

--- cachematrix.R
+++ cachematrix.R
@@ -42,5 +42,23 @@ list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 #this function can compute the inverse of the matrix or get the inverse by checking the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ ## Return a matrix that is the inverse of 'x'
+  mat <- x$getInverse()
+  
+  if(!is.null(mat)){
+    message("inverse found in cache")
+    return(mat)
+  }
+  #fetch matrix
+  data <- x$get()
+  
+  #computing inverse of the matrix
+  mat <- solve(data) %% data
+  
+  #set inverse   
+  x$setInverse(mat)
+  
+  #returns the matrix 
+  mat
+  
 }
